b93c426421818a288a54e38bc764414f369d86aa
Theodor Stefan Jurubita <theodor.jurubita@gmail.com>
2016-08-15 22:46:27 +0300

    Creating Project

---
+++ CRMCarDealer/CRMCarDealer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRMCarDealer.MvcApplication" Language="C#" %>
---
+++ CRMCarDealer/CRMCarDealer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRMCarDealer.Startup))]
+namespace CRMCarDealer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
