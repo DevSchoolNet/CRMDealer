c30b1e698eaf1d13293a629cb62abe218a57ea76
Theodor Stefan Jurubita <theodor.jurubita@gmail.com>
2016-09-19 15:39:45 +0300

    Afisare DriveTests din baza de date

---
+++ CRMCarDealer/CRMCarDealer/Controllers/DriveTestController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessServices;
+using CRMCarDealer.Models;
+using CRMModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace CRMCarDealer.Controllers
+{
+    public class DriveTestController:Controller
+    {
+        public CRMContext _context;
+        public DriveTestController()
+        {
+            _context = new CRMContext();
+        }
+
+        public ActionResult Index()
+        {
+            var driveTests = _context.DriveTests.Include(d => d.Salesman).Include(d => d.Prospect).Include(d => d.TestCar).ToList();
+            return View(driveTests);
+        }
+    }
+}
